9a81cefd7fabbca1e359c5a123e3ffd2ebbd74fa
Syuq <tatd.wine@gmail.com>
2022-01-09 19:35:38 +0700

    modified config.h dmenu

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
+	"JetBrainsMono Nerd Font:size=10",
 	"JetBrainsMono Nerd Font:pixelsize=8:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
