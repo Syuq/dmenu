032813cf5675413a815277b8e2076a2e5241a35b
Syuq <tatd.wine@gmail.com>
2022-03-14 20:16:38 +0700

    chore: modified font

--- config.h
+++ config.h
@@ -4,9 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=10",
-	"JetBrainsMono Nerd Font:pixelsize=8:antialias=true:autohint=true"
-};
+    /* "JetBrainsMono Nerd Font:size=10", */
+    /* "JetBrainsMono Nerd Font:pixelsize=8:antialias=true:autohint=true" */
+    "monospace:size=10",
+    "JoyPixels:pixelsize=8:antialias=true:autohint=true"};
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
